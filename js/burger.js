2e1d914f16a269c400dc301b07329c97650adfd2
Torin Mallett <torin.mallett@netmatters-scs.com>
2026-01-20 09:49:40 +0000

    Added Scss to side menu and removed sidebar html from header

--- js/burger.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector(".menu-toggle");
-const sidebar = document.querySelector(".sidebar-items");
-
-menuToggle.addEventListener("click", () => {
-    sidebar.classList.toggle("active");
-});
